958952af394173e24e8bed2a4157a5877795486b
Darpan <darpandeka6000@gmail.com>
2024-05-24 00:11:11 +0530

    cart added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, {useState} from "react";
 import Header from "./components/Header/header"
 import Products from "./components/products/Products"
+import Cart from "./components/cart/Cart";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+    const openCart = () => setShowCart(true);
+    const closeCart = () => setShowCart(false);
   return (
     <div>
-      <Header />
+      <Header openCart={openCart}/>
       <Products />
+      <Cart showCart={showCart} closeCart={closeCart}/>
     </div>
   );
 }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "./Header.css";
+import Modal from "../UI/modal";
 
-function Header() {
+function Header({openCart}) {
+    // const [showCart, setShowCart] = useState(false);
+
+    // const openCart = () => setShowCart(true);
+    // const closeCart = () => setShowCart(false);
     return (
         <div className="header">
             <h1>My e-commerce app</h1>
+            <div>
+                <button className="yellow-button" onClick={openCart}>
+                    Cart
+                </button>
+            </div>
         </div>
     );
 }
